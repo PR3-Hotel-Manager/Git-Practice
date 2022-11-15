ef9ed6510610783bd7a89e7de2a79476ad097033
michaelgranberry <michaelgranberryii@gmail.com>
2022-11-14 23:57:52 -0800

    added bye func

--- MikesClass.py
+++ MikesClass.py
@@ -1,7 +1,9 @@
 class MikesClass:
     def SayHi():
-        print("hello")
+        print("hello from mike's class")
 
+    def SayBye():
+        print("Bye from mike's class!")
 
 def main():
     print("Hello World!")
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import MikesClass as mc
 def main():
     print("Hello World!")
     mc.MikesClass.SayHi()
+    mc.MikesClass.SayBye()
 
 if __name__ == "__main__":
     main()
